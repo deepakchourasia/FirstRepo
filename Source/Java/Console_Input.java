4c0a3f0658f998a549af6d0a269f67c44fce810e
Deepak1 <Deepak.Chourasia@sas.com>
2015-07-09 10:48:42 +0530

    Committing the folder

---
+++ Source/Java/Console_Input.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Console_Input {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		try {
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Enter first number : ");
+			a = scanner.nextInt();
+			System.out.print("Enter second number : ");
+			b = scanner.nextInt();
+			System.out.println("sum is : " + Integer.toString(a + b));
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
